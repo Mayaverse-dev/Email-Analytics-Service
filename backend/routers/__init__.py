b7b2f8a25cbeb3df2bf7a6b8ea2e213582aa28b6
Anmol Tripathi <anmol.ratan.tripathi@gmail.com>
2026-04-02 16:58:33 +0530

    fixes

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,7 +1,8 @@
-from . import broadcasts, contacts, dashboard, segment_folders, segments, sync, users, webhooks
+from . import broadcasts, cleanup, contacts, dashboard, segment_folders, segments, sync, users, webhooks
 
 __all__ = [
     "broadcasts",
+    "cleanup",
     "contacts",
     "dashboard",
     "segment_folders",
